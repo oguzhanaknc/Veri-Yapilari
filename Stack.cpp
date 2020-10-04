d072592bc18c8cd8230e6c1e3aed49547fe6363d
oguzhan <oguzzhanakinci@gmail.com>
2020-10-04 21:28:23 +0300

    Stack (Yığın) Eklendi.

---
+++ Stack.cpp
@@ -0,0 +1,107 @@
+/*
+ * Author : Oğuzhan Akıncı
+ *      29.09.2020
+ * Email: oguzhanaknc06@gmail.com
+ */
+
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+template<class T>
+class  LinkedList{
+private:
+    // düğüm yapımızı oluşturduğumuz kısım. ( kutular )
+    struct n{
+        T x;
+        n* next;
+    };
+    typedef n node;
+    node * root = (node *)malloc(sizeof(node)); //kök kutumuzu oluşturuyoruz.
+    node * iter = (node *)malloc(sizeof(node)); // gezgin kutumuz.
+    node * tail = (node *)malloc(sizeof(node)); // Sondan bir önceki kutu.
+public:
+    // Construactor fonksyonumuzda kök elemanımızın değerlerini atıyor ve gezgin kutumuzu root'a eşitliyoruz.
+    LinkedList(){
+        root->next = NULL;
+        root->x = NULL;
+        iter = root;
+        tail = root;
+    }
+    //sona veri ekleme fonksyonu. parametre olarak eklenecek veriyi alıyor.
+    void Push(T data){
+        if (root->x == NULL){ //eğer daha önce veri eklenmemişse onu root'a ver diyoruz.
+            root->x = data;
+        }else{ //eğer veri eklenmişse
+            while (iter->next != NULL){ //son elemana kadar gezgin kutumuz gezsin.
+                iter = iter->next;
+            }
+            iter->next = (node *)malloc(sizeof(node)); //ve en sona yeni bir düğüm (kutu) oluştursun.
+            iter->next->x = data; //oluşan kutuya değerleri atayalım.
+            tail = iter; // son kutuyu kaydırıyoruz.
+            iter->next->next = NULL;
+        }
+        resetIter(); // gezgin kutumuzu tekrar root'a eşitleyeylim.
+    }
+    //ekrana yazdırma fonksyonu parametresi yok.
+    void yaz(){
+        resetIter();
+        while (iter != NULL){ //son elemana kadar gezgin node (kutu) ilerliyor.
+            cout << iter->x << endl; //her elemanın değerini yazdırıyor.
+            iter = iter->next;
+        }
+
+    }
+    // gezgin elemanı başa alma fonksyonu.
+    void resetIter(){
+        iter = root;
+    }
+    // Pop() Son veriyi sil ve döndür.
+    T LinkedList<T>::Pop()  {
+        resetIter(); //gezgin kutuyu sıfırla
+        if (root->x == NULL){ //liste dolu mu boş mu bak.
+            cout << "Boş Stack Silinemez..." << endl;
+            return -1;
+        }
+        node * temp; //temp oluştur
+        T tempdata;
+
+        while (iter->next != tail){ //tailden önceki kutuya ilerle
+            iter = iter->next;
+        }
+        temp = tail->next; // tailden sonraki kutuyu temp'e al
+        tempdata = tail->next->x; //datayı al
+        tail = iter; //kuyruğu kaydır
+        tail->next->next = NULL; // atamayı yap
+        free(temp); //temp'i hafızadan sil
+        resetIter(); // gezgini sıfırla
+        return tempdata; // değeri döndür
+
+
+    }
+};
+
+
+
+
+
+
+int main() {
+    //tam sayılarla işlem yapmak
+    LinkedList<int> liste;
+    liste.Push(9);
+    liste.Push(98);
+    liste.Push(12);
+    liste.Push(182);
+    liste.Push(99); // bağlı listeye eleman eklemek.
+    liste.yaz();
+    cout << "*/*/*/*/*/*/" << endl;
+    cout << liste.Pop() << endl; // veri silme fonksyonu
+    cout << "*/*/*/*/*/*/" << endl;
+    liste.yaz();
+
+    return 0;
+}
